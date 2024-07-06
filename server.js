436a773761e086d4d8494880c454e39e0fa61a27
anirutsolrak <carloseduardoturina@gmail.com>
2024-07-06 20:55:08 -0300

    correção de config

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const jsonServer = require('json-server');
+// JSON Server module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+// Redirecionando todas as requisições GET para /produtos
+server.use(jsonServer.rewriter({
+  '/': '/videos' 
+}));
+
 server.use(router);
-server.listen(3001, () => {
-  console.log('JSON Server is running');
+
+// Listen to port
+server.listen(3000, () => {
+  console.log("JSON Server is running");
 });
+
+// Export the Server API
+module.exports = server;
