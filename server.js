d54687f6e72e865d10c26e422f824680fcef6ddd
Carlos Eduardo Turina <146773453+anirutsolrak@users.noreply.github.com>
2024-07-08 10:52:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
 
-// Make sure to use the default middleware
+// Ler o conteúdo de db.json
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+
+// Configurar o roteamento
+const router = jsonServer.router(db); // Passar os dados lidos para o router
+
+// Usar o middleware padrão
 const middlewares = jsonServer.defaults();
 
+// Criar o servidor
+const server = jsonServer.create();
+
+// Adicionar o middleware
 server.use(middlewares);
 
-// Redirecionando todas as requisições GET para /produtos
+// Redirecionar requisições GET para /videos
 server.use(jsonServer.rewriter({
-  "/": "/videos" 
+  "/": "/videos"
 }));
 
+// Usar o roteador
 server.use(router);
 
-// Listen to port
+// Iniciar o servidor na porta 3000
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log('JSON Server is running');
 });
 
-// Export the Server API
+// Exportar o servidor API
 module.exports = server;
